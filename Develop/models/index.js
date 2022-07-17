9a087db0fcec1f4a60744c7234228664f2edfaca
xndroli <iamsimplyclean@knights.ucf.edu>
2022-07-17 17:14:27 -0400

    init commit of /develop/models/index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,32 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: ProductTag,
+  as: "",
+  foreignKey: "",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Product.belongToMany(Tag, {
+  through: ProductTag,
+  as: "",
+  foreignKey: "",
+});
 
 module.exports = {
   Product,
